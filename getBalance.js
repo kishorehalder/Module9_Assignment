aecc206dc31a2fe40054d7e6cf5960fc0ad74bb4
kishorehalder <144475346+kishorehalder@users.noreply.github.com>
2023-11-05 05:47:29 +0600

    Update getBalance.js

--- getBalance.js
+++ getBalance.js
@@ -1,1 +1,7 @@
+async function getBalance(account) {
+  const web3 = new Web3(window.ethereum);
+  const balanceWei = await web3.eth.getBalance(account);
+  const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
+  return balanceEth;
+}
 
